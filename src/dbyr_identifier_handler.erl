f3654e8ad71e0fd4d8c0bfa416d7ae2bfddf8f02
marcsugiyama <sugiyama@acm.org>
2015-05-11 15:44:56 -0700

    Send headers to allow cross domain

--- src/dbyr_identifier_handler.erl
+++ src/dbyr_identifier_handler.erl
@@ -9,6 +9,7 @@
          allow_missing_post/2,
          known_methods/2,
          allowed_methods/2,
+         options/2,
          content_types_provided/2,
          content_types_accepted/2,
          delete_resource/2,
@@ -31,13 +32,18 @@ allow_missing_post(Req, State) ->
     {true, Req, State}.
 
 known_methods(Req, State) -> 
-    {[<<"GET">>,<<"POST">>,<<"DELETE">>], Req, State}.
+    {[<<"OPTIONS">>,<<"GET">>,<<"POST">>,<<"DELETE">>], Req, State}.
 
 allowed_methods(Req, State) ->
-    {[<<"GET">>,<<"POST">>,<<"DELETE">>], Req, State}.
+    {[<<"OPTIONS">>,<<"GET">>,<<"POST">>,<<"DELETE">>], Req, State}.
 
 content_types_provided(Req, State) ->
     {[{<<"application/json">>, handle_json}], Req, State}.
+
+options(Req0, State) ->
+    Req1 = cowboy_req:set_resp_header(<<"Access-Control-Allow-Headers">>,
+                                                    <<"content-type">>, Req0),
+    {ok, Req1, State}.
 
 resource_exists(Req0, #{identifier := Identifier} = State) ->
     case dbyr_identifier:get_metadata(Identifier) of
@@ -45,7 +51,8 @@ resource_exists(Req0, #{identifier := Identifier} = State) ->
             {ok, Req1} = cowboy_req:reply(500, [], stringify(Reason), Req0),
             {stop, Req1, State};
         [] ->
-            {false, Req0, State};
+            Req1 = set_cross_domain(Req0),
+            {false, Req1, State};
         Metadata ->
             {true, Req0, State#{exists := true, metadata := Metadata}}
     end.
@@ -56,7 +63,8 @@ delete_resource(Req0, #{identifier := Identifier} = State) ->
             {ok, Req1} = cowboy_req:reply(500, [], stringify(Reason), Req0),
             {stop, Req1, State};
         ok ->
-            {true, Req0, State}
+            Req1 = set_cross_domain(Req0),
+            {true, Req1, State}
     end.
 
 content_types_accepted(Req, State) ->
@@ -68,12 +76,13 @@ handle_json(Req0, State) ->
 
 handle_json_method(Req, #{exists := false} = State, <<"GET">>) ->
     {false, Req, State};
-handle_json_method(Req, #{exists := true,
+handle_json_method(Req0, #{exists := true,
                          identifier := Identifier,
                          metadata := Metadata} = State, <<"GET">>) ->
-    {dbyr_identifier:to_json(Identifier, Metadata), Req, State};
-handle_json_method(Req, #{identifier := Identifier} = State, <<"POST">>) ->
-    {Metadata, Req1} = case cowboy_req:body(Req) of
+    Req1 = set_cross_domain(Req0),
+    {dbyr_identifier:to_json(Identifier, Metadata), Req1, State};
+handle_json_method(Req0, #{identifier := Identifier} = State, <<"POST">>) ->
+    {Metadata, Req1} = case cowboy_req:body(Req0) of
         {ok, <<>>, R1} ->
             {[], R1};
         {ok, Body, R1} ->
@@ -82,12 +91,17 @@ handle_json_method(Req, #{identifier := Identifier} = State, <<"POST">>) ->
     end,
     case dbyr_identifier:publish(Identifier, Metadata) of
         ok ->
-            {{true, dbyr_identifier:to_resource(Identifier)},
-                cowboy_req:set_resp_body(<<"true">>, Req1), State};
+            Req2 = set_cross_domain(Req1),
+            Req3 = cowboy_req:set_resp_body(<<"true">>, Req2),
+            {{true, dbyr_identifier:to_resource(Identifier)}, Req3, State};
         {error, Reason} ->
             ?ERROR("publish error: ~p~n", [Reason]),
-            {false, Req1, State}
+            Req2 = set_cross_domain(Req1),
+            {false, Req2, State}
     end.
 
 stringify(Reason) ->
-    iolist_to_binary(io_lib:format("~p", [Reason])).
+    io_lib:format("~p", [Reason]).
+
+set_cross_domain(Req) ->
+    cowboy_req:set_resp_header(<<"Access-Control-Allow-Origin">>, <<"*">>, Req).
